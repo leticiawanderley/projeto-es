a169fc055b90ea90064948556d428c7290c21045
João Pedro Leôncio <joopeeds@gmail.com>
2014-07-20 15:29:30 -0300

    Extracting Routes constants

---
+++ android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/utils/Routes.java
@@ -0,0 +1,10 @@
+package br.edu.ufcg.maonamassa.utils;
+
+public class Routes {
+	public final static String SERVER_URL = "https://mao-na-massa.appspot.com";
+	public final static String CREATE_ROUTE = "create";
+	public final static String UPDATE_ROUTE = "update";
+	public final static String DELETE_ROUTE = "delete";
+	public final static String SEARCH_ROUTE = "search";
+
+}
--- android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/utils/Storable.java
+++ android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/utils/Storable.java
@@ -26,16 +26,11 @@ import com.google.gson.*;
 
 public abstract class Storable<Recipe> {
 	
-	private final String SERVER_URL = "https://mao-na-massa.appspot.com";
-	private final String CREATE_ROUTE = "create/";
-	private final String UPDATE_ROUTE = "update/";
-	private final String DELETE_ROUTE = "delete/";
-	private final String SEARCH_ROUTE = "search/";
-
+	
 	public void create(User user) throws Exception {
 		String result = jsonify();
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(CREATE_ROUTE + "?");
+		stringBuilder.append(Routes.CREATE_ROUTE + "?");
 		stringBuilder.append("what=" + this.getClass().getName());
 		stringBuilder.append("user_id=" + user.getId());
 		stringBuilder.append("token=" + user.getAccessToken());
@@ -66,7 +61,7 @@ public abstract class Storable<Recipe> {
 		return result;
 	}
 	
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings("rawtypes")
 	public List<Recipe> desjsonifyList(String jsonstr) {
 		Gson json = new GsonBuilder().serializeNulls().create();
 		Type fooType = new TypeToken<Recipe>() {}.getType();
@@ -81,7 +76,7 @@ public abstract class Storable<Recipe> {
 	
 	private String sendRequest(String handlerArguments) throws Exception {
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(SERVER_URL);
+		stringBuilder.append(Routes.SERVER_URL);
 		stringBuilder.append("/");
 		stringBuilder.append(handlerArguments);
 		String url = stringBuilder.toString();
@@ -92,7 +87,7 @@ public abstract class Storable<Recipe> {
 	public void update(User user) throws Exception{
 		String result = jsonify();
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(UPDATE_ROUTE + "?");
+		stringBuilder.append(Routes.UPDATE_ROUTE + "?");
 		stringBuilder.append("what=" + this.getClass().getName());
 		stringBuilder.append("user_id=" + user.getId());
 		stringBuilder.append("token=" + user.getAccessToken());
@@ -105,7 +100,7 @@ public abstract class Storable<Recipe> {
 	public void delete(User user) throws Exception{
 		String result = jsonify();
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(DELETE_ROUTE + "?");
+		stringBuilder.append(Routes.DELETE_ROUTE + "?");
 		stringBuilder.append("what=" + this.getClass().getName());
 		stringBuilder.append("user_id=" + user.getId());
 		stringBuilder.append("token=" + user.getAccessToken());
@@ -116,7 +111,7 @@ public abstract class Storable<Recipe> {
 	
 	public List<Recipe> search(StorableQuery query) throws Exception{
 		StringBuilder stringBuilder = new StringBuilder();
-		stringBuilder.append(SEARCH_ROUTE + "?");
+		stringBuilder.append(Routes.SEARCH_ROUTE + "?");
 		stringBuilder.append("where=" + query.getWhere());
 		String desc = query.isDesc()?"true":"false";
 		stringBuilder.append("desc=" + desc);

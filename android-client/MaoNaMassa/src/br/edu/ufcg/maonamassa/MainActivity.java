59d1ebad039eee9d5da69e37b85e5c2be6c25f9b
rodrigofariasm <rodrigohfarias@gmail.com>
2014-07-19 22:25:07 -0300

    criado button addrecipe

--- android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/MainActivity.java
+++ android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/MainActivity.java
@@ -4,13 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
-	public final static String EXTRA_MESSAGE = "br.edu.MaoNaMassa.MESSAGE";	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,29 +22,39 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    	 MenuInflater inflater = getMenuInflater();
+    	 inflater.inflate(R.menu.main, menu);
+    	 return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+        case R.id.action_add_recipe:
+        	addRecipe();
+        	return true;
+        case R.id.action_settings:
+            openSettings();
             return true;
+        default:
+            return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
-    
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	System.out.println(message);
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
-    }
+
+
+	private void addRecipe() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private void openSettings() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
 }

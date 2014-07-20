6c0f333145e3c729a678262f085476dbd3788e9e
João Pedro Leôncio <joopeeds@gmail.com>
2014-07-20 00:37:51 -0300

    Storable is now only for Recipe

--- android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/utils/Storable.java
+++ android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/utils/Storable.java
@@ -1,53 +1,90 @@
 package br.edu.ufcg.maonamassa.utils;
 
 import java.lang.reflect.Type;
+import java.util.List;
+
+import br.edu.ufcg.maonamassa.models.User;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.*;
 import com.google.gson.*;
 
-
+/***
+ * @author JoaoPedro
+ * This class must be ONLY inherited by Recipe. 
+ * All other classes are stored by hierarchy.
+ * An Recipe object when is stored, also stores its Step objects.
+ * Storable abstracts the create, update, delete and retrieve operations
+ *
+ * @param <Recipe>
+ */
 /*
- * Classe que deve ser herdada por todos os objetos que irão ser armazenados 
- * na Datastore do Google App Engine server. Essa classe abstrai a create, update e delete.
- * Breve colocarei mais detalhes de como funcionará, porque ainda não pensei.
+ * 
  */
 
-public abstract class Storable<T> {
-
-	public void create(){
-		
-		String result = jsonify();
+public abstract class Storable<Recipe> {
 	
-		String handlerArguments = "create?what=" + this.getClass().getName() + "json=" + result;
+	private final String SERVER_URL = "https://mao-na-massa.appspot.com";
+	private final String CREATE_ROUTE = "create/";
+	private final String UPDATE_ROUTE = "update/";
+	private final String DELETE_ROUTE = "delete/";
+	private final String SEARCH_ROUTE = "search/";
+
+	public void create(User user){
+		String result = jsonify();
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(CREATE_ROUTE + "?");
+		stringBuilder.append("what=" + this.getClass().getName());
+		stringBuilder.append("user_id=" + user.getId());
+		stringBuilder.append("token=" + user.getAccessToken());
+		stringBuilder.append("json=" + result);
+		String handlerArguments = stringBuilder.toString();
 		
 		sendRequest(handlerArguments);
 	}
 
 	public String jsonify() {
-		Type fooType = new TypeToken<T>() {}.getType();
+		Type fooType = new TypeToken<Recipe>() {}.getType();
 		Gson json = new GsonBuilder().serializeNulls().create();
 		String result = json.toJson(this, fooType);
 		return result;
 	}
 	
-	public T desjsonify(String jsonstr) {
+	public Recipe desjsonify(String jsonstr) {
 		Gson json = new GsonBuilder().serializeNulls().create();
 		@SuppressWarnings("unchecked")
-		T fromJson = (T) json.fromJson(jsonstr, this.getClass());
+		Recipe fromJson =  (Recipe) json.fromJson(jsonstr, this.getClass());
 		return fromJson;
 	}
 	
 	private void sendRequest(String handlerArguments) {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(SERVER_URL);
+		stringBuilder.append("/");
+		stringBuilder.append(handlerArguments);
+		String url = stringBuilder.toString();
 		// TODO Envia o request para o server com os argumentos
 	}
 
-	public void update(){
+	public void update(User user){
+		String result = jsonify();
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(CREATE_ROUTE + "?");
+		stringBuilder.append("what=" + this.getClass().getName());
+		stringBuilder.append("user_id=" + user.getId());
+		stringBuilder.append("token=" + user.getAccessToken());
+		stringBuilder.append("json=" + result);
+		String handlerArguments = stringBuilder.toString();
+		
+		sendRequest(handlerArguments);
+	}
+	
+	public void delete(User user){
 		
 	}
 	
-	public void delete(){
-		
+	public List<Recipe> search(User user){
+		return null;
 	}
 	
 }

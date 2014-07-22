d85237bb37e610634c24541386c143c167546459
rodrigofariasm <rodrigohfarias@gmail.com>
2014-07-22 04:04:18 -0300

    vendo ingredients, aprendi list view finalmente

---
+++ android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/IngredientItem.java
@@ -0,0 +1,20 @@
+package br.edu.ufcg.maonamassa;
+
+public class IngredientItem {
+
+    private String ingredient;
+
+    public IngredientItem(String ingredient) {
+        this.ingredient = ingredient;
+       }
+
+     public String getingredient() {
+          return ingredient;
+      }
+
+    public void setingredient(String ingredient) {
+         this.ingredient = ingredient;
+    }
+
+
+}
--- android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/SeeRecipeActivity.java
+++ android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/SeeRecipeActivity.java
@@ -1,19 +1,31 @@
 package br.edu.ufcg.maonamassa;
 
-import br.edu.ufcg.maonamassa.models.Recipe;
+import java.util.List;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 import android.widget.TextView;
+import br.edu.ufcg.maonamassa.models.Recipe;
 
 public class SeeRecipeActivity extends ActionBarActivity {
+	
 
+	private List<IngredientItem> ingredientItems;
+	private IngredientsAdapter adapter;
+	private ListView ingredientsView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,14 +35,18 @@ public class SeeRecipeActivity extends ActionBarActivity {
 					.add(R.id.container_ver_receita, new PlaceholderFragment()).commit();
 		}
 		
+		ingredientsView = (ListView) findViewById(R.id.ingredient_list);
 		Intent i = getIntent();
 		Recipe recipe = new Recipe(0L, "NULL", null);
 		recipe = recipe.desjsonify(i.getStringExtra("Recipe"));
-		
+		recipe.addIngredient("novoIngrediente");
+		recipe.addIngredient("novoIngrediente2");
 		TextView title = (TextView)findViewById(R.id.textView1); // title
         TextView author = (TextView)findViewById(R.id.textView2); // author name
         title.setText(recipe.getName());
         author.setText(recipe.getAuthor().getName());
+        IngredientsAdapter ingredientsAdapter = new IngredientsAdapter(this, recipe.getIngredients());
+        ingredientsView.setAdapter(ingredientsAdapter); 
 
 	}
 
@@ -41,7 +57,9 @@ public class SeeRecipeActivity extends ActionBarActivity {
 		getMenuInflater().inflate(R.menu.add_colecao, menu);
 		return true;
 	}
-
+	
+	
+	 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will

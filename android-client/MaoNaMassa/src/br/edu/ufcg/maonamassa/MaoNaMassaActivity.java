1c1a534b32c2e77b5c4ba246833124c7b10bc9b1
leticiawanderley <leticia.wanderley@ccc.ufcg.edu.br>
2014-08-13 22:43:39 -0300

    Changing wrong string

--- android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/MaoNaMassaActivity.java
+++ android-client/MaoNaMassa/src/br/edu/ufcg/maonamassa/MaoNaMassaActivity.java
@@ -2,8 +2,6 @@ package br.edu.ufcg.maonamassa;
 
 import java.util.Iterator;
 
-import br.edu.ufcg.maonamassa.models.Recipe;
-import br.edu.ufcg.maonamassa.models.Step;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Fragment;
@@ -11,190 +9,193 @@ import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v4.app.NavUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
+import br.edu.ufcg.maonamassa.models.Recipe;
+import br.edu.ufcg.maonamassa.models.Step;
 
 @SuppressLint("NewApi")
-public class MaoNaMassaActivity extends Activity implements FragmentManager.OnBackStackChangedListener{
-	
+public class MaoNaMassaActivity extends Activity implements
+		FragmentManager.OnBackStackChangedListener {
+
 	private Handler mHandler = new Handler();
-	private boolean mShowingBack  = false;
+	private boolean mShowingBack = false;
 	private Recipe recipe;
 	private Iterator<Step> steps;
-	
+
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_mao_na_massa);
-        
-        Intent i = getIntent();
+		setContentView(R.layout.activity_mao_na_massa);
+
+		Intent i = getIntent();
 		recipe = new Recipe(0L, "NULL", null);
 		recipe = recipe.desjsonify(i.getStringExtra("Recipe"));
-        
-        steps = recipe.getSteps().iterator();
-        
-        
-		
+
+		steps = recipe.getSteps().iterator();
+
 		CardFragment fragment = new CardFragment();
-		if(steps.hasNext())
-		 fragment.setStep(steps.next());
-        
-        if (savedInstanceState == null) {
-            // If there is no saved instance state, add a fragment representing the
-            // front of the card to this activity. If there is saved instance state,
-            // this fragment will have already been added to the activity.
-            getFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.container, fragment)
-                    .commit();
-        } else {
-            mShowingBack = (getFragmentManager().getBackStackEntryCount() > 0);
-        }
+		if (steps.hasNext())
+			fragment.setStep(steps.next());
 
-        // Monitor back stack changes to ensure the action bar shows the appropriate
-        // button (either "photo" or "info").
-        getFragmentManager().addOnBackStackChangedListener(this);
-    }
-	
-	
-	 @Override
-	    public boolean onCreateOptionsMenu(Menu menu) {
-	        super.onCreateOptionsMenu(menu);
-	        /*
-	        // Add either a "photo" or "finish" button to the action bar, depending on which page
-	        // is currently selected.
-	        MenuItem item = menu.add(Menu.NONE, R.id.action_flip, Menu.NONE,
-	                mShowingBack
-	                        ? R.string.action_photo
-	                        : R.string.action_info);
-	        item.setIcon(mShowingBack
-	                ? R.drawable.ic_action_search
-	                : R.drawable.ic_action_search);
-	        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-	        */
-	        return true;
-	    }
+		if (savedInstanceState == null) {
+			// If there is no saved instance state, add a fragment representing
+			// the
+			// front of the card to this activity. If there is saved instance
+			// state,
+			// this fragment will have already been added to the activity.
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, fragment).commit();
+		} else {
+			mShowingBack = (getFragmentManager().getBackStackEntryCount() > 0);
+		}
 
-	    @Override
-	    public boolean onOptionsItemSelected(MenuItem item) {
-	        switch (item.getItemId()) {
-	            case R.id.action_flip:
-	                flipCard();
-	                return true;
-	        }
+		// Monitor back stack changes to ensure the action bar shows the
+		// appropriate
+		// button (either "photo" or "info").
+		getFragmentManager().addOnBackStackChangedListener(this);
+	}
 
-	        return super.onOptionsItemSelected(item);
-	    }
-	
-   
-	    private void flipCard() {
-	        if (mShowingBack) {
-	            getFragmentManager().popBackStack();
-	            return;
-	        }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		/*
+		 * // Add either a "photo" or "finish" button to the action bar,
+		 * depending on which page // is currently selected. MenuItem item =
+		 * menu.add(Menu.NONE, R.id.action_flip, Menu.NONE, mShowingBack ?
+		 * R.string.action_photo : R.string.action_info);
+		 * item.setIcon(mShowingBack ? R.drawable.ic_action_search :
+		 * R.drawable.ic_action_search);
+		 * item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		 */
+		return true;
+	}
 
-	        // Flip to the back.
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_flip:
+			flipCard();
+			return true;
+		}
 
-	        mShowingBack = true;
+		return super.onOptionsItemSelected(item);
+	}
 
-	        CardFragment fragment = new CardFragment();
-	        if(steps.hasNext())
-	         fragment.setStep(steps.next());
-	        
-	        // Create and commit a new fragment transaction that adds the fragment for the back of
-	        // the card, uses custom animations, and is part of the fragment manager's back stack.
+	private void flipCard() {
+		if (mShowingBack) {
+			getFragmentManager().popBackStack();
+			return;
+		}
 
-	        getFragmentManager()
-	                .beginTransaction()
+		// Flip to the back.
 
-	                // Replace the default fragment animations with animator resources representing
-	                // rotations when switching to the back of the card, as well as animator
-	                // resources representing rotations when flipping back to the front (e.g. when
-	                // the system Back button is pressed).
-	                .setCustomAnimations(
-	                        R.animator.card_flip_right_in, R.animator.card_flip_right_out,
-	                        R.animator.card_flip_left_in, R.animator.card_flip_left_out)
+		mShowingBack = true;
 
-	                // Replace any fragments currently in the container view with a fragment
-	                // representing the next page (indicated by the just-incremented currentPage
-	                // variable).
-	                .replace(R.id.container, fragment)
+		CardFragment fragment = new CardFragment();
+		if (steps.hasNext())
+			fragment.setStep(steps.next());
 
-	                // Add this transaction to the back stack, allowing users to press Back
-	                // to get to the front of the card.
-	                .addToBackStack(null)
+		// Create and commit a new fragment transaction that adds the fragment
+		// for the back of
+		// the card, uses custom animations, and is part of the fragment
+		// manager's back stack.
 
-	                // Commit the transaction.
-	                .commit();
+		getFragmentManager().beginTransaction()
 
-	        
-	        // Defer an invalidation of the options menu (on modern devices, the action bar). This
-	        // can't be done immediately because the transaction may not yet be committed. Commits
-	        // are asynchronous in that they are posted to the main thread's message loop.
-	         mHandler.post(new Runnable() {
-	            @Override
-	            public void run() {
-	                invalidateOptionsMenu();
-	            }
-	        }); 
-	    }
+		// Replace the default fragment animations with animator resources
+		// representing
+		// rotations when switching to the back of the card, as well as animator
+		// resources representing rotations when flipping back to the front
+		// (e.g. when
+		// the system Back button is pressed).
+				.setCustomAnimations(R.animator.card_flip_right_in,
+						R.animator.card_flip_right_out,
+						R.animator.card_flip_left_in,
+						R.animator.card_flip_left_out)
 
-	    @Override
-	    public void onBackStackChanged() {
-	        mShowingBack = (getFragmentManager().getBackStackEntryCount() > 0);
+				// Replace any fragments currently in the container view with a
+				// fragment
+				// representing the next page (indicated by the just-incremented
+				// currentPage
+				// variable).
+				.replace(R.id.container, fragment)
 
-	         //When the back stack changes, invalidate the options menu (action bar).
-	        invalidateOptionsMenu();
-	    }
+				// Add this transaction to the back stack, allowing users to
+				// press Back
+				// to get to the front of the card.
+				.addToBackStack(null)
 
-	    
-	    
-    /**
-     * A fragment representing the front of the card.
-     */
-    public class CardFragment extends Fragment {
-    	
-    	Step step;
-    	
-    	public void setStep(Step step){
-    		this.step = step;
-    	}
-    	
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-        	View rootView = inflater.inflate(R.layout.fragment_card_front, container, false);
-        	TextView t  = (TextView) rootView.findViewById(R.id.nome_etapa);
-        	Button b  = (Button) rootView.findViewById(R.id.button_next_step);
-        	if(step !=null)
-        	b.setOnClickListener(new OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					flipCard();
-				}
-			});
-        	String title = step != null? step.getDescription(): "";
-        	String titleButton = step != null? "PrÛxima": "Terminar!";
-        	t.setText(title);
-        	b.setText(titleButton);
-        	return rootView;
-        }
-    }
+				// Commit the transaction.
+				.commit();
 
-    
-    
+		// Defer an invalidation of the options menu (on modern devices, the
+		// action bar). This
+		// can't be done immediately because the transaction may not yet be
+		// committed. Commits
+		// are asynchronous in that they are posted to the main thread's message
+		// loop.
+		mHandler.post(new Runnable() {
+			@Override
+			public void run() {
+				invalidateOptionsMenu();
+			}
+		});
+	}
+
+	@Override
+	public void onBackStackChanged() {
+		mShowingBack = (getFragmentManager().getBackStackEntryCount() > 0);
+
+		// When the back stack changes, invalidate the options menu (action
+		// bar).
+		invalidateOptionsMenu();
+	}
+
+	/**
+	 * A fragment representing the front of the card.
+	 */
+	@SuppressLint("ValidFragment")
+	public class CardFragment extends Fragment {
+
+		Step step;
+
+		public void setStep(Step step) {
+			this.step = step;
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_card_front,
+					container, false);
+			TextView t = (TextView) rootView.findViewById(R.id.nome_etapa);
+			Button b = (Button) rootView.findViewById(R.id.button_next_step);
+			if (step != null)
+				b.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						flipCard();
+					}
+				});
+			String title = step != null ? step.getDescription() : "";
+			String titleButton = step != null ? "Pr√≥xima" : "Terminar!";
+			t.setText(title);
+			b.setText(titleButton);
+			return rootView;
+		}
+	}
+
 }
